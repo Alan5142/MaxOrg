813449e666dd8b1a647fad4c571f057215bde1e3
Alan Ramírez Herrera <alan5142@hotmail.com>
2018-11-21 20:40:53 -0600

    Dialog now scrolls

--- MaxOrg/ClientApp/src/app/start-page/user-start/user-start.component.ts
+++ MaxOrg/ClientApp/src/app/start-page/user-start/user-start.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {MatIconRegistry, MatDialog} from '@angular/material';
 import {DomSanitizer} from '@angular/platform-browser';
-import { NewProjectComponent } from '../new-project/new-project.component';
-import { FlexAlignDirective } from '@angular/flex-layout';
+import {NewProjectComponent} from '../new-project/new-project.component';
+import {FlexAlignDirective} from '@angular/flex-layout';
 
 @Component({
   selector: 'app-user-start',
@@ -17,17 +17,20 @@ export class UserStartComponent implements OnInit {
     'Usuario2/Proyecto3'
   ];
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog) {
+  }
+
   openDialog(): void {
     const dialogRef = this.dialog.open(NewProjectComponent, {
       width: '50%',
-      minWidth:'300px'
+      minWidth: '300px'
     });
 
     dialogRef.afterClosed().subscribe(() => {
       console.log('The dialog was closed');
     });
   }
+
   ngOnInit() {
   }
 
