913e909e7e9bafb1784e42169adea8b21d5f2aa8
luis Martínez <lucas.2451@hotmail.com>
2019-05-21 23:06:34 -0500

    assign work section displays users

--- MaxOrg/ClientApp/src/app/project/assigned-work/assigned-work.component.ts
+++ MaxOrg/ClientApp/src/app/project/assigned-work/assigned-work.component.ts
@@ -1,11 +1,12 @@
-import {AfterViewInit, Component, OnInit, ViewChildren} from '@angular/core';
-import {MediaObserver} from '@angular/flex-layout';
-import {MatDialog, MatTab, MatTabGroup, MatSnackBar} from '@angular/material';
-import {AssignWorkComponent} from './assign-work/assign-work.component';
+import { AfterViewInit, Component, OnInit, ViewChildren, ViewChild } from '@angular/core';
+import { MediaObserver } from '@angular/flex-layout';
+import { MatDialog, MatTab, MatTabGroup, MatSnackBar, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { AssignWorkComponent } from './assign-work/assign-work.component';
 import { ProjectsService } from 'src/app/services/projects.service';
 import { ActivatedRoute } from '@angular/router';
 import { TasksService, Task } from 'src/app/services/tasks.service';
 import { map } from 'rxjs/operators';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-assigned-work',
@@ -16,62 +17,105 @@ import { map } from 'rxjs/operators';
 export class AssignedWorkComponent implements OnInit, AfterViewInit {
   @ViewChildren(MatTabGroup) group;
   @ViewChildren(MatTab) tabs;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  displayedColumns: string[] = ['username', 'actions'];
+  usersDisplay: MatTableDataSource<any>;
   tab_num = 0;
   selected = 0;
-  SWIPE_ACTION = {LEFT: 'swipeleft', RIGHT: 'swiperight'};
-  groups;
-  adminGroups=[];
-  adminGroupsFlat=[];
-  object={};
+  SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
+  groups=[]; groupsFlat=[];
+  adminGroups = [];  adminGroupsFlat = [];
+  membergroups=[];
   userId;
-  taskGroupId;
+  taskTarget;
   url;
-  constructor(public snackBar:MatSnackBar,public mediaObserver: MediaObserver, public dialog: MatDialog,
-     public route:ActivatedRoute, public projectService:ProjectsService, private taskService:TasksService) {
-    this.userId=localStorage.getItem('userId');
-    this.taskGroupId=localStorage.getItem("taskGroup");
+  users = [];
+  admin=false;
+  constructor(public snackBar: MatSnackBar, public mediaObserver: MediaObserver, public dialog: MatDialog,
+    public route: ActivatedRoute, public projectService: ProjectsService, private taskService: TasksService) {
+    this.userId = localStorage.getItem('userId');
+    this.taskTarget = localStorage.getItem("taskTarget");
     this.route.parent.params.subscribe(params => {
-      this.url="/project/"+params['id']+"/requirements";
+      this.url = "/project/" + params['id'] + "/requirements";
       this.projectService.getProject(params['id']).subscribe(project => {
-        this.groups=project;
-        if(this.groups.groupOwner==this.userId)
-          this.adminGroups=this.groups.subgroups;
+        this.groups.push(project);
+        if (this.groups[0].groupOwner == this.userId)
+          this.adminGroups.push(project);
         else
-          this.groups.subgroups.forEach(group => {
+          this.groups[0].subgroups.forEach(group => {
             this.getAdminGroups(group);
           });
-        this.flat(this.adminGroups);
+        this.flat(this.adminGroups,this.adminGroupsFlat);
+        this.getMembers(this.adminGroupsFlat);
+        this.usersDisplay = new MatTableDataSource(this.users);
+        this.flat(this.groups,this.groupsFlat);
+        this.getMemberGroups(this.groupsFlat);
+        console.log(this.membergroups);
         console.log(this.adminGroupsFlat);
+        console.log(this.users);
+        if(this.adminGroupsFlat.length>0)
+          this.admin=true;
+        this.usersDisplay.sort = this.sort;
+        this.usersDisplay.paginator = this.paginator;
       })
     });
+
+  }
+  ngOnInit() {
     
   }
-  getAdminGroups(group){
-    if(group.groupOwner==this.userId){
+  getMemberGroups(groups){
+    groups.forEach(group => {
+      group.members.forEach(member => {
+        if(member.key==this.userId)
+          this.membergroups.push({
+            name: group.name,
+            id: group.id,
+            members: group.members,
+            tasks: this.taskService.getGroupTasks(group.id)
+          });
+      });
+    });
+  }
+  getAdminGroups(group) {
+    if (group.groupOwner == this.userId) {
       this.adminGroups.push(group);
     }
-    else{
+    else {
       group.subgroups.forEach(group => {
         this.getAdminGroups(group);
       });
     }
   }
-  flat(toFlat){
-    toFlat.forEach((group) => {
-      this.adminGroupsFlat.push({
-        name:group.name,
-        id:group.id,
-        members:group.members,
-        tasks: this.taskService.getGroupTasks(group.id)
+  getMembers(groupArray){
+    groupArray.forEach(group => {
+      group.members.forEach(member => {
+        let repeat = false;
+        this.users.forEach(user => {
+          if (user.key == member.key)
+            repeat = true;
+        });
+        if (!repeat){
+          this.users.push(member);
+          if(member.key==this.taskTarget)
+            this.openAssignTask(this.taskTarget,true);  
+        }
       });
-      this.flat(group.subgroups);
-      if(this.taskGroupId==group.id)
-        this.openAssignTask(this.taskGroupId,this.adminGroupsFlat.length-1);
     });
   }
-  
-  ngOnInit() {
-      
+  flat(toFlat,flatArray) {
+    toFlat.forEach((group) => {
+      flatArray.push({
+        name: group.name,
+        id: group.id,
+        members: group.members,
+        tasks: this.taskService.getGroupTasks(group.id)
+      });
+      this.flat(group.subgroups,flatArray);
+      if (this.taskTarget == group.id)
+        this.openAssignTask(this.taskTarget,false, this.adminGroupsFlat.length - 1);
+    });
   }
 
   ngAfterViewInit() {
@@ -88,24 +132,29 @@ export class AssignedWorkComponent implements OnInit, AfterViewInit {
     }
   }
 
-  openAssignTask(groupId:string,index:number) {
-    localStorage.setItem("url",this.url);
+  openAssignTask(id: string, isUser:boolean, index?: number) {
+    localStorage.setItem("url", this.url);
     console.log(this.url);
     const dialogRef = this.dialog.open(AssignWorkComponent, {
       width: '50%',
       maxWidth: '500px',
       minWidth: '250px',
-      data: groupId
+      data: {id:id,isUser:isUser}
     });
-    dialogRef.afterClosed().subscribe(r=>{
-      if(r){
-        r.subscribe(
-          r=>this.adminGroupsFlat[index].tasks=
-          this.adminGroupsFlat[index].tasks.pipe(map<Task[],any>(tasks=>{return tasks;}))
-        );
-        this.snackBar.open("Tarea agregada","cerrar");
+    dialogRef.afterClosed().subscribe(r => {
+      if (r) {
+        if(!isUser)
+          this.adminGroupsFlat[index].tasks =
+          this.adminGroupsFlat[index].tasks.pipe(map<Task[], any>(tasks => { return tasks; }))
+        this.snackBar.open("Tarea agregada", "cerrar");
       }
     });
   }
-  
+  applyFilter(filterValue: string) {
+    this.usersDisplay.filter = filterValue.trim().toLowerCase();
+    if (this.usersDisplay.paginator) {
+      this.usersDisplay.paginator.firstPage();
+    }
+  }
+
 }
